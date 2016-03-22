2b6967cb2f1daf5b97bbd47a0b3e4bbf75d3d596
bingqiw <wangbq5@asiainfo.com>
2016-03-22 22:09:42 +0800

    UPATE 001

--- GGG/src/com/HelloWorld.java
+++ GGG/src/com/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GGG2");
-		
-
+		System.out.println("GGG2 UPDATE 001");
+	}
 }
