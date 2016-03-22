efa8bc16e1bbfcbc8ccceb6e17a31119c2de800a
bingqiw <wangbq5@asiainfo.com>
2016-03-22 23:01:01 +0800

    MASTER

--- GGG/src/com/HelloWorld.java
+++ GGG/src/com/HelloWorld.java
@@ -4,9 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GGG2");
-		System.out.println("GGG2 UPDATE 001");
-		System.out.println("GGG2 UPDATE 002");
-		System.out.println("GGG2 UPDATE 003");
+		System.out.println("MASTER");		
 	}
 }
