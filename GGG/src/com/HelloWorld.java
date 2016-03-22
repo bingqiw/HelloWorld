9d0597c600cfe44470c673a221876fe825b4072e
bingqiw <wangbq5@asiainfo.com>
2016-03-22 22:35:51 +0800

    UPDATE 3

--- GGG/src/com/HelloWorld.java
+++ GGG/src/com/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
 		System.out.println("GGG2");
 		System.out.println("GGG2 UPDATE 001");
 		System.out.println("GGG2 UPDATE 002");
+		System.out.println("GGG2 UPDATE 003");
 	}
 }
