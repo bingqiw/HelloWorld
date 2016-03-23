8ff36126742eb9d5a47c80da4957ef84a8a3018b
bingqiw <wangbq5@asiainfo.com>
2016-03-23 14:31:45 +0800

    GGG3 HELLOWORLD 14:31

--- GGG/src/com/HelloWorld2.java
+++ GGG/src/com/HelloWorld2.java
@@ -5,7 +5,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GGG3 HELLOWORLD2");	
-		System.out.println("GGG3 HELLOWORLD2 14:25");	
+		System.out.println("GGG3 HELLOWORLD2 14:28");	
 		
 	}
 }
--- GGG/src/com/HelloWorld3.java
+++ GGG/src/com/HelloWorld3.java
@@ -4,6 +4,6 @@ public class HelloWorld3{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("MASTER CONSOLE UPDATE");	
+		System.out.println("GGG3 HELLOWORLD3 14:29");	
 	}
 }
