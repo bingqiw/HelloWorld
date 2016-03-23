8c4c7d3100e7a86246200e09d4421f5b82f072b9
bingqiw <wangbq5@asiainfo.com>
2016-03-23 16:53:01 +0800

    dele

--- GGG/src/com/HelloWorld2.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class HelloWorld2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("GGG3 HELLOWORLD2 16:45");	
-	}
-}
--- GGG/src/com/HelloWorld3.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class HelloWorld3{
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("GGG3 HELLOWORLD3 16:32");	
-	}
-}
