9651bc9d92e4c7ab2c6ccd2e851270c197413a48
bingqiw <wangbq5@asiainfo.com>
2016-03-23 14:44:50 +0800

    Update HelloWorld3.java

--- GGG/src/com/HelloWorld3.java
+++ GGG/src/com/HelloWorld3.java
@@ -5,5 +5,6 @@ public class HelloWorld3{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GGG3 HELLOWORLD3 14:29");	
+		System.out.println("GGG3 HELLOWORLD3 14:45");	
 	}
 }
