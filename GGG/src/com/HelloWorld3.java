02ea058dfbecf1d0ce3265742b60c3f74c3df8b1
bingqiw <wangbq5@asiainfo.com>
2016-03-23 15:40:37 +0800

    13:40

--- GGG/src/com/HelloWorld3.java
+++ GGG/src/com/HelloWorld3.java
@@ -5,6 +5,6 @@ public class HelloWorld3{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GGG3 HELLOWORLD3 14:29");	
-		System.out.println("GGG3 HELLOWORLD3 15:38");
+		System.out.println("GGG3 HELLOWORLD3 15:40");
 	}
 }
